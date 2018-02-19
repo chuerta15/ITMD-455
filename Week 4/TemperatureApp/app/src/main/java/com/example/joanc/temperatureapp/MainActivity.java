2873e636f8a2108e421db0c580ee2c1ad9b88169
chuerta15 <chuerta@hawk.iit.edu>
2018-02-19 15:55:29 -0600

    Initial Commit

---
+++ Week 4/TemperatureApp/app/src/main/java/com/example/joanc/temperatureapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.joanc.temperatureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Seekbar seekbar;
+    //imageView imageview;
+
+    //int progress =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //seekbar = (imageview)findViewById(R.id.imageView2);
+    //textview.setText(''+progress);
+
+}
