dbcbc80d4e0da90bdfa4b7f29587191079846255
chuerta15 <chuerta@hawk.iit.edu>
2018-02-23 01:26:18 -0600

    Added all the Files

--- Week 5/CitiesListViewLab/app/src/main/java/com/example/joanc/citieslistviewlab/MainActivity.java
+++ Week 5/CitiesListViewLab/app/src/main/java/com/example/joanc/citieslistviewlab/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.joanc.citieslistviewlab;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ListAdapter;
+        import android.widget.ListView;
+        import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-
+    String ListItemsName[] = new String[]{ "Bangalore", "Beijing",
+            "Chicago"};
+    Integer ImageName[] = { R.drawable.bangalore, R.drawable.beijing,
+            R.drawable.chicago};
+    ListView listView;
+    ListAdapter listAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.citylist);
+        listAdapter = new CustomListAdapot(MainActivity.this,
+                ListItemsName, ImageName);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ListItemsName[position],
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
