2496841ba36c2b7ff37a83081c9d3b842bb95da6
chuerta15 <chuerta@hawk.iit.edu>
2018-03-19 15:17:46 -0500

    Changes to Week 6

--- Week 6/app/src/main/java/com/example/joanc/myboundservicetimerapp/MainActivity.java
+++ Week 6/app/src/main/java/com/example/joanc/myboundservicetimerapp/MainActivity.java
@@ -5,9 +5,29 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStartServices;
+    Button btnStopServices;
+    Button btnStart;
+    Button btnPause;
+    Button btnReset;
+    TextView txtTimer;
+    Handler customHandler = new Handler();
+    LinearLayout container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartServices = (Button)findViewById(R.id.btnStartServices);
+        btnStopServices = (Button)findViewById(R.id.btnStopServices);
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnPause = (Button)findViewById(R.id.btnPause);
+        btnStop = (Button)findViewById(R.id.btnStop);
+        txtTimer = (TextView)findViewById(R.id.timerValue);
+
+        }
+
+
     }
 }
